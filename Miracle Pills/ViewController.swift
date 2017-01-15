c25411e11e2632f7b27ee07527e1b614c08b49e4
carlosBabel <carlos.babel@gmail.com>
2017-01-15 20:18:37 +0100

    lesson 42

--- Miracle Pills/ViewController.swift
+++ Miracle Pills/ViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var paisPicker: UIPickerView!
+    @IBOutlet weak var paisPickerBoton: UIButton!
+    let paises = ["España","Portugal","Francia","Italia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        paisPicker.dataSource = self
+        paisPicker.delegate = self
+        
+        self.view.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func presionarBotonPaisPicker(_ sender: Any) {
+        paisPicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return paises.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return paises[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        paisPickerBoton.setTitle(paises[row], for: UIControlState.normal)
+        paisPicker.isHidden = true
+    }
 
 }
 
